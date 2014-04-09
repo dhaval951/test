8b8d891c27cca7570055aa082b41cd67caaa9ede
Dhaval Bhadania <dhaval.bhadania@ZS-10.local>
2014-04-09 16:42:33 +0530

    Initial Commit

---
+++ test/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Dhaval Bhadania on 09/04/14.
+//  Copyright Dhaval Bhadania 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ test/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test
+//
+//  Created by Dhaval Bhadania on 09/04/14.
+//  Copyright Dhaval Bhadania 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
